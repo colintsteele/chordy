7032736a0f728c41737e3ee0b6f8a6b26ec30a3f
Colin Steele <colin.steele20@gmail.com>
2024-03-19 12:29:56 -0400

    Revert "Find memory leak"
    This reverts commit ee444da76c8505f2ddc445dfa9ebdd328320827a.

--- src/services/ToneService.ts
+++ src/services/ToneService.ts
@@ -1,40 +1,42 @@
-import * as Tone from "tone";
-import PianoMp3 from "tonejs-instrument-piano-mp3";
-import MidiNote from "../midi/MidiNote";
 
-const instrument = new PianoMp3();
+import * as Tone from 'tone';
+import PianoMp3 from 'tonejs-instrument-piano-mp3';
+import MidiNote from '../midi/MidiNote';
+
+const instrument = new PianoMp3;
 instrument.toMaster();
 
 class ToneService {
-  static playSound() {
-    //create a synth and connect it to the main output (your speakers)
-    const synth = new Tone.Synth().toDestination();
+    static playSound() {
 
-    instrument.toMaster();
-    instrument.triggerAttackRelease("C3", "4n");
-    instrument.triggerAttackRelease("C4", "4n");
-  }
+        //create a synth and connect it to the main output (your speakers)
+        const synth = new Tone.Synth().toDestination();
 
-  static playNote(midiNumber: number) {
-    let note = new MidiNote(midiNumber).octaveNote;
-    let noteArg = `${note.noteName}${note.octave}`;
+        instrument.toMaster();
+        instrument.triggerAttackRelease("C3", '4n');
+        instrument.triggerAttackRelease("C4", '4n');
+    }
 
-    instrument.triggerAttackRelease(noteArg, "4n");
-  }
+    static playNote(midiNumber: number) {
+        let note = new MidiNote(midiNumber).octaveNote;
+        let noteArg = `${note.noteName}${note.octave}`
 
-  static pressNote(midiNumber: number) {
-    let note = new MidiNote(midiNumber).octaveNote;
-    let noteArg = `${note.noteName}${note.octave}`;
+        instrument.triggerAttackRelease(noteArg,'4n');
+    }
 
-    instrument.triggerAttack(noteArg);
-  }
+    static pressNote(midiNumber: number) {
+        let note = new MidiNote(midiNumber).octaveNote;
+        let noteArg = `${note.noteName}${note.octave}`
 
-  static liftNote(midiNumber: number) {
-    let note = new MidiNote(midiNumber).octaveNote;
-    let noteArg = `${note.noteName}${note.octave}`;
+        instrument.triggerAttack(noteArg);
+    }
 
-    instrument.triggerRelease(noteArg);
-  }
+    static liftNote(midiNumber: number) {
+        let note = new MidiNote(midiNumber).octaveNote;
+        let noteArg = `${note.noteName}${note.octave}`
+
+        instrument.triggerRelease(noteArg);
+    }
 }
 
 export default ToneService;
